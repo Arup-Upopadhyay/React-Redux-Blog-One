b5a4b27b3b40032ab0fb3a903405932237c8f3c1
arup.upopadhyay <arup.upopadhyay@gmail.com>
2019-12-29 10:56:40 +0530

    Working React-redux Application.

--- src/containers/activeUser.js
+++ src/containers/activeUser.js
@@ -2,15 +2,18 @@ import React  from 'react';
 import {connect} from  'react-redux';
 
 const ActiveUser = (props) => {
+    const style = {
+        textAlign: 'center'
+    }
     if(!props.activeUser) {
         return(
-            <div>Please select a user!!</div>
+            <div style={style}>Please select a user!!</div>
         )
     }
     else{
         return(
-            <div>
-                `you clicked on {props.activeUser.first} {props.activeUser.last}`
+            <div style={style}>
+                you clicked on {props.activeUser.first} {props.activeUser.last}
             </div>
         )
     }
--- src/reducers/activeUser.js
+++ src/reducers/activeUser.js
@@ -1,6 +1,8 @@
+import {USER_SELECTED} from '../actions/actionList';
+
 export default (state = null ,action) => {
     switch (action.type) {
-        case 'USER_SELECTED':
+        case USER_SELECTED:
             return action.payload;
         default :
             return state;
