96160507210284c40a27050748829c897939f8da
arup.upopadhyay <arup.upopadhyay@gmail.com>
2019-12-30 08:33:47 +0530

    Working React-redux Application with remote user fetch.

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import {createStore, applyMiddleware} from 'redux';
 import allReducers from './reducers'; 
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-const store = createStore(allReducers , applyMiddleware(thunk));
+const store = createStore(allReducers , applyMiddleware(thunk , logger));
 
 export default store;
